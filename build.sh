031c47317529ede77587c75c52807fa8cf01a935
Niels Baltodano <niels.baltodano@gmail.com>
2018-05-15 12:19:20 -0500

    firts commit develop

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -x
+rm -rf build || true
+rm -rf my-java-app || true
+NAMETEMPLATE="mvnjava"
+VERSIONTEMPLATE="1.0"
+
+FILEZIP="mvnjava-template-1.0.zip"
+USERBINTRAY="niels58"
+KEYBINTRAY="b089fdb7d14138cbe431b4decc39cbe8b2e5321d"
+REPOBINTRAY="lazybonestest"
+FILEBINTRAY="work"
+BASEAPIBINTRAY="api.bintray.com/content"
+
+URLREPOBINTRAY="https://$BASEAPIBINTRAY/$USERBINTRAY/$REPOBINTRAY/$FILEBINTRAY/$VERSIONTEMPLATE/$NAMETEMPLATE-templates-$VERSIONTEMPLATE.zip"
+gradle installAllTemplates --stacktrace --debug --info
+
+cd build/packages && curl -T $FILEZIP \
+  -u$USERBINTRAY:$KEYBINTRAY \
+   $URLREPOBINTRAY
+
+#lazybones create mvnjava 1.0 my-java-app
